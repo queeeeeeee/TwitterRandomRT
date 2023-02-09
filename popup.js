ccdf68b7919dc6e2c75397d84fb91072441524d3
queeeeeeee <124815524+queeeeeeee@users.noreply.github.com>
2023-02-09 16:58:19 +0900

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,42 +1,42 @@
 async function getActiveTabURL() {
-  const tabs = await chrome.tabs.query({
-      currentWindow: true,
-      active: true
-  });
+    const tabs = await chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    });
 
-  return tabs[0];
+    return tabs[0];
 }
 
 const OnResult = (result) => {
-  document.getElementsByName("result")[0].innerHTML = result;
+    // document.getElementsByName("result")[0].innerHTML = result;
 };
 
-const OnRandom = async () => {
-  const activeTab = await getActiveTabURL();
+const OnRandom = async() => {
+    const activeTab = await getActiveTabURL();
 
-  chrome.tabs.sendMessage(activeTab.id, {
-    type: "Random",
-  }, OnResult);
+    chrome.tabs.sendMessage(activeTab.id, {
+        type: "Random",
+    }, OnResult);
 }
 
-document.addEventListener("DOMContentLoaded", async () => {
-  const activeTab = await getActiveTabURL();
+document.addEventListener("DOMContentLoaded", async() => {
+    const activeTab = await getActiveTabURL();
 
-  if (activeTab.url.includes("twitter.com") && activeTab.url.includes("retweets")) {
-    const container = document.getElementsByName("container")[0];
-    container.innerHTML = '';
+    if (activeTab.url.includes("twitter.com") && activeTab.url.includes("retweets")) {
+        const container = document.getElementsByName("container")[0];
+        container.innerHTML = '';
 
-    var button = document.createElement('button');
-    button.onclick = OnRandom;
-    button.innerHTML = "추첨하기";
-    container.appendChild(button);
+        var button = document.createElement('button');
+        button.onclick = OnRandom;
+        button.classList.add("button-17")
+        button.innerHTML = "추첨하기";
+        container.appendChild(button);
 
-  } else {
-    const container = document.getElementsByName("container")[0];
-    container.innerHTML = '트윗의 리트윗 페이지를 열어주세요.';
-    var button = document.getElementsByClassName("RandomButton")[0];
-    if(button != null)
-        button.parentNode.removeChild(button);
-  }
+    } else {
+        const container = document.getElementsByName("container")[0];
+        container.innerHTML = '트윗의 리트윗 페이지를 열어주세요.';
+        var button = document.getElementsByClassName("RandomButton")[0];
+        if (button != null)
+            button.parentNode.removeChild(button);
+    }
 });
-
