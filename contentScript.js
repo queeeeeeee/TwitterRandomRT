6d98a0044b34f9ac082556b079e89e3bcfc9c348
queeeeeeee <124815524+queeeeeeee@users.noreply.github.com>
2023-02-10 00:08:52 +0900

    Add files via upload

--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,17 @@
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; 
+    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
 }
 
 function sleep(ms) {
     return new Promise((r) => setTimeout(r, ms));
+}
+
+
+function getRandomItem(set) {
+    let items = Array.from(set);
+    return items[Math.floor(Math.random() * items.length)];
 }
 
 async function OnRandom(response) {
@@ -34,9 +40,21 @@ async function OnRandom(response) {
 
     element.parentNode.parentNode.scroll(0, getRandomInt(0, prevY));
     await sleep(500);
-    var find = element.querySelectorAll('[data-testid="cellInnerDiv"]');
 
-    var index = getRandomInt(0, find.length);
+
+    var find = element.querySelectorAll('[data-testid="cellInnerDiv"]');
+    var numbers = new Set();
+    for (var i = 0; i < find.length; i++) {
+        numbers.add(i);
+    }
+
+    var index = getRandomItem(numbers);
+
+    if (find[index].getElementsByClassName('span').length == 0) {
+        numbers.delete(index);
+        index = getRandomItem(numbers);
+    }
+
     find[index].scrollIntoView();
     await sleep(500);
     find[index].style.backgroundColor = "dimgrey";
