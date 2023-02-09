063aec5caf2dd9e266fd7261bda679ee9f19e90e
queeeeeeee <124815524+queeeeeeee@users.noreply.github.com>
2023-02-09 16:58:04 +0900

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,33 +1,47 @@
-
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; 
+}
+
+function sleep(ms) {
+    return new Promise((r) => setTimeout(r, ms));
+}
+
+async function OnRandom(response) {
+
+    var find = document.querySelectorAll('[aria-label="Timeline: Retweeted by"]');
+    if (find.length == 0) {
+        find = document.querySelectorAll('[aria-label="타임라인: 리트윗함"]');
+    }
+    var element = find[0];
+
+    var prevY = 0;
+
+    element.parentNode.parentNode.scroll(0, 0);
+    await sleep(500);
+    while (true) {
+        var currentY = element.parentNode.parentNode.scrollTop + 200;
+
+        if (currentY == prevY)
+            break;
+        element.parentNode.parentNode.scroll(0, currentY);
+
+        await sleep(500);
+
+        prevY = currentY;
+    }
+
+    element.parentNode.parentNode.scroll(0, getRandomInt(0, prevY));
+    await sleep(500);
+    var find = element.querySelectorAll('[data-testid="cellInnerDiv"]');
+
+    var index = getRandomInt(0, find.length);
+    find[index].scrollIntoView();
+    await sleep(500);
+    find[index].style.backgroundColor = "dimgrey";
 }
 
 chrome.runtime.onMessage.addListener((obj, sender, response) => {
-
-  var find = document.querySelectorAll('[aria-label="Timeline: Retweeted by"]');
-  if(find.length == 0)
-  {
-    find = document.querySelectorAll('[aria-label="타임라인: 리트윗함"]');
-  }
-  var element = find[0];
-  //find every span in child of elements
-  var spans = element.getElementsByTagName("span");
-  var arr = [];
-  var index = 0;
-
-  for(var i = 0; i<spans.length;i++)
-  {
-    if(spans[i].innerHTML.startsWith('@'))
-    {
-        arr[index++] = spans[i].innerHTML;
-    }
-  }
-  var randomIndex = getRandomInt(0, arr.length);
-  var result = arr[randomIndex];
-
-  response(result);
+    OnRandom(response);
 });
-
